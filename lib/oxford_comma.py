a9fdc1c43fce44ae04e4518489b74be5da3d28f9
kgrace61 <110522366+kgrace61@users.noreply.github.com>
2024-04-06 15:52:32 -0500

    result

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,20 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        return ", ".join(items[:-1]) + ", and " + items[-1]
+
+
+#check if the length is 0 or 1: if length is 0, return empty string
+#if length is 1, return the only element as a string
+# if > 1, create a new empty array to store the result
+# #iterate over items(array of strings) using a loop
+#append each element to the result string, followed by a comma and space
+#if the element is the second to last element, append "and" instead of a comma and a space
+#append last element to the string
+#return the string
+
